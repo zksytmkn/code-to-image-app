2fb18a59a2c8159fca5ae5339929fc01af38babf
nakamoto <zksytmkn4869@gmail.com>
2023-12-30 19:34:26 +0900

    ✨

---
+++ src/lib/generate-image.ts
@@ -0,0 +1,25 @@
+import { domToDataUrl, domToBlob } from "modern-screenshot";
+
+const options = {
+  scale: 4,
+};
+
+export const generateImage = async (blob?: boolean): Promise<string | Blob> => {
+  const element = document.getElementById("canvas")!;
+  const width = element.offsetWidth;
+  const height = element.offsetHeight;
+
+  if (width && height && height * (16 / 9) > width) {
+    element.style.width = `${height * (16 / 9)}px`;
+  }
+
+  const result = blob ? await domToBlob(element, options) : await domToDataUrl(element, options);
+
+  element.style.width = "auto";
+
+  if (blob) {
+    return result;
+  }
+
+  return result;
+};
